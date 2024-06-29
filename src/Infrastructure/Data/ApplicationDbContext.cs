ad0a2a299b70bc808320206cad44619df308f6e4
Tomasz Plewniak <t_plewniak@yahoo.com>
2024-06-29 23:05:31 +0200

    Add base entity and context.

---
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using ApplicationCore.TodoItems;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<TodoItemEntity> TodoItems { get; set; }
+}
