63e65c4c281567ddb54f73ddafcdc54b7325e4cd
Tomasz Plewniak <t_plewniak@yahoo.com>
2024-06-29 23:12:25 +0200

    Add base DI of the services.

---
+++ src/ApplicationCore/ConfigureServices.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ApplicationCore;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddApplicationCoreServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        return services;
+    }
+}
---
+++ src/Infrastructure/ConfigureServices.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddInfrastructureServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        return services;
+    }
+}
